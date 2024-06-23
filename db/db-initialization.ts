c1ca5b2be861cb3ee6f9750fced722b498dff1e1
Maayan <maayansonnenfeld@gmail.com>
2024-06-23 22:24:52 +0300

    Db initialization

--- db/db-initialization.ts
+++ db/db-initialization.ts
@@ -24,159 +24,67 @@ const createTables = async (): Promise<void> => {
         const res = await query(`SELECT NOW();`);
         console.log("Set time zone. Current time: ", res.rows[0].now);
 
-
-        // Create the customers table without foreign key constraint
-        await query(`CREATE TABLE IF NOT EXISTS customers (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      phone VARCHAR(20) UNIQUE NOT NULL,
-      name TEXT,
-      address UUID,
-      last_used_addresses UUID[],
-      has_ordered_before BOOLEAN DEFAULT FALSE
+// Create the users table
+        await query(`CREATE TABLE IF NOT EXISTS users (
+      user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      phone_number VARCHAR(15) UNIQUE NOT NULL,
     );`);
 
-        // Create the addresses table without foreign key constraint
-        await query(`CREATE TABLE IF NOT EXISTS addresses (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      maker UUID,
-      contact VARCHAR(20),
-      open_till TIME,
-      formatted TEXT,
-      city VARCHAR(100),
-      street VARCHAR(100),
-      street_nm VARCHAR(100),
-      lat VARCHAR(20),
-      lng VARCHAR(20),
-      postal_code TEXT,
-      city_id TEXT,
-      district TEXT
+        // Create the categories table
+        await query(`CREATE TABLE IF NOT EXISTS categories (
+      category_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id UUID NOT NULL REFERENCES users(user_id),
+      category_name VARCHAR(100) NOT NULL
     );`);
 
-        // Add foreign key constraints to customers and addresses
-        await query(`
-      DO $$ 
-      BEGIN 
-        IF NOT EXISTS (
-          SELECT 1 
-          FROM pg_constraint 
-          WHERE conname = 'fk_customers_address'
-        ) THEN
-          ALTER TABLE customers ADD CONSTRAINT fk_customers_address FOREIGN KEY (address) REFERENCES addresses(id);
-        END IF;
-      END $$;
-    `);
-        await query(`
-      DO $$ 
-      BEGIN 
-        IF NOT EXISTS (
-          SELECT 1 
-          FROM pg_constraint 
-          WHERE conname = 'fk_addresses_maker'
-        ) THEN
-          ALTER TABLE addresses ADD CONSTRAINT fk_addresses_maker FOREIGN KEY (maker) REFERENCES customers(id);
-        END IF;
-      END $$;
-    `);
-
-
-        // Create the reviews table
-        await query(`CREATE TABLE IF NOT EXISTS reviews (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      grade FLOAT,
-      comment TEXT      
+        // Create the fields table
+        await query(`CREATE TABLE IF NOT EXISTS fields (
+      field_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      category_id UUID NOT NULL REFERENCES categories(category_id),
+      field_name VARCHAR(100) NOT NULL
     );`);
 
-
-        // Create the couriers table
-        await query(`CREATE TABLE IF NOT EXISTS couriers (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      phone VARCHAR(20) UNIQUE NOT NULL,
-      active_orders VARCHAR(20)[],
-      completed_orders VARCHAR(20)[],
-      area TEXT[],
-      grade FLOAT,
-      reviews UUID[]
+        // Create the links table
+        await query(`CREATE TABLE IF NOT EXISTS links (
+      link_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id UUID NOT NULL REFERENCES users(user_id),
+      url TEXT NOT NULL,
+      extra_text TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`);
 
-
-        // Create the orders table
-        await query(`CREATE TABLE IF NOT EXISTS orders (
-      id VARCHAR(20) PRIMARY KEY,
-      package_size TEXT,
-      pickup_date TIMESTAMP,
-      dropoff_date TIMESTAMP,
-      price_for_customer INTEGER,
-      price_for_courier INTEGER,
-      customer UUID REFERENCES customers(id),
-      courier UUID REFERENCES couriers(id),
-      pickup_address UUID REFERENCES addresses(id),
-      dropoff_address UUID REFERENCES addresses(id),
-      is_intercity BOOLEAN DEFAULT FALSE,
-      order_received_in TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      comments TEXT,
-      status VARCHAR(20)
+        // Create the contacts table
+        await query(`CREATE TABLE IF NOT EXISTS contacts (
+      contact_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id UUID NOT NULL REFERENCES users(user_id),
+      contact_name VARCHAR(100),
+      phone_number VARCHAR(15),
+      email VARCHAR(100),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`);
 
-
-        // Create the completed_orders table
-        await query(`CREATE TABLE IF NOT EXISTS completed_orders (
-      id VARCHAR(20) PRIMARY KEY,
-      final_comment TEXT,
-      price_for_customer INTEGER,
-      price_for_courier INTEGER,
-      customer UUID REFERENCES customers(id),
-      pickup_address UUID REFERENCES addresses(id),
-      dropoff_address UUID REFERENCES addresses(id),
-      courier UUID REFERENCES couriers(id),
-      is_intercity BOOLEAN DEFAULT FALSE,
-      order_received_in TIMESTAMP,
-      delivered_in TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      comments TEXT
+        // Create the notes table
+        await query(`CREATE TABLE IF NOT EXISTS notes (
+      note_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id UUID NOT NULL REFERENCES users(user_id),
+      note_text TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`);
 
-
-        // Add foreign key constraints to completed orders, orders, and couriers
-        // Currently commented out due to problem with enforcing such constraints on arrays
-    //     await query(`
-    //   DO $$
-    //   BEGIN
-    //     IF NOT EXISTS (
-    //       SELECT 1
-    //       FROM pg_constraint
-    //       WHERE conname = 'fk_courier_orders'
-    //     ) THEN
-    //       ALTER TABLE couriers ADD CONSTRAINT fk_courier_orders FOREIGN KEY (active_orders) REFERENCES orders(id);
-    //     END IF;
-    //   END $$;
-    // `);
-    //     await query(`
-    //   DO $$
-    //   BEGIN
-    //     IF NOT EXISTS (
-    //       SELECT 1
-    //       FROM pg_constraint
-    //       WHERE conname = 'fk_courier_completed_orders'
-    //     ) THEN
-    //       ALTER TABLE couriers ADD CONSTRAINT fk_courier_completed_orders FOREIGN KEY (completed_orders) REFERENCES completed_orders(id);
-    //     END IF;
-    //   END $$;
-    // `);
-
-
-        // Create the courier_groups table
-        await query(`CREATE TABLE IF NOT EXISTS courier_groups (
-      serialized_id TEXT PRIMARY KEY,
-      tags TEXT[][]
+        // Create the category_entries table
+        await query(`CREATE TABLE IF NOT EXISTS category_entries (
+      entry_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      category_id UUID NOT NULL REFERENCES categories(category_id),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`);
 
-
-        // Create the ad_timers table
-        await query(`CREATE TABLE IF NOT EXISTS ad_timers (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      advertise_time TIMESTAMP,
-      order_to_advertise VARCHAR(20) REFERENCES orders(id)
-    );`)
-
+        // Create the entry_fields table
+        await query(`CREATE TABLE IF NOT EXISTS entry_fields (
+      entry_field_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      entry_id UUID NOT NULL REFERENCES category_entries(entry_id),
+      field_id UUID NOT NULL REFERENCES fields(field_id),
+      field_value TEXT NOT NULL
+    );`);
 
         console.log('Tables created successfully');
     } catch (err) {
